eda194e943d2d6835e2f62ca253d519d8d825ad2
NMHK-134711 <khg950520@gmail.com>
2025-05-29 02:49:46 +0900

    Add files via upload

---
+++ src/turtlebot3_term_pj/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'turtlebot3_term_pj'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hk',
+    maintainer_email='hk@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+        'console_scripts': [
+            'teleop_gui = turtlebot3_term_pj.teleop_gui:main',
+        ],
+        'console_scripts': [
+            'patrol_node = turtlebot3_term_pj.patrol_node:main',
+        ],
+    },
+)
